226e61219e00b82b427178d2a1c9539957add6ed
Sebastian Yang <yukaiy@andrew.cmu.edu>
2022-08-15 19:49:20 -0700

    Base Code (URP+AR)

---
+++ Assets/_Scripts/ObjectPlacer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ObjectPlacer : MonoBehaviour
+{
+    public GameObject objectToPlacePrefab;
+    public GameObject cursorObject;
+    public ARRaycastManager raycastManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCursor();
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            //Instantiate(objectToPlacePrefab, transform.position, transform.rotation);
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            raycastManager.Raycast(Input.GetTouch(0).position, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+            if (hits.Count > 0)
+            {
+                Instantiate(objectToPlacePrefab, hits[0].pose.position, hits[0].pose.rotation);
+            }
+        }
+
+        void UpdateCursor()
+        {
+            Vector2 screenPoint = Camera.main.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+            raycastManager.Raycast(screenPoint, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+            if (hits.Count > 0)
+            {
+                transform.position = hits[0].pose.position;
+                transform.rotation = hits[0].pose.rotation;
+            }
+        }
+    }
+}
