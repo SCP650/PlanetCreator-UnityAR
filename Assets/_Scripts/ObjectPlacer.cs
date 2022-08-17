6c96859db64dc70ad61e5659f738073fa6e47317
Sebastian Yang <yukaiy@andrew.cmu.edu>
2022-08-16 21:28:53 -0700

    update ui

--- Assets/_Scripts/ObjectPlacer.cs
+++ Assets/_Scripts/ObjectPlacer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.EventSystems;
 
 public class ObjectPlacer : MonoBehaviour
 {
@@ -20,6 +21,7 @@ public class ObjectPlacer : MonoBehaviour
         //UpdateCursor();
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
+            if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)) return;
             //Instantiate(objectToPlacePrefab, transform.position, transform.rotation);
             List<ARRaycastHit> hits = new List<ARRaycastHit>();
             raycastManager.Raycast(Input.GetTouch(0).position, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
