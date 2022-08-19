ec066045aec4de8f9f90f5aa076c00b3d3f14ef5
Sebastian Yang <yukaiy@andrew.cmu.edu>
2022-08-18 22:16:38 -0700

    create sphere

---
+++ Assets/_Scripts/PlanetGenerator/PlanetGen.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGen : MonoBehaviour
+{
+    [Range(2, 256)]
+    public int resolution = 10;
+
+    public ShapeSettings shapeSettings;
+    public ColorSettings colorSettings;
+
+    [SerializeField, HideInInspector]
+    MeshFilter[] meshFilters;
+    TerrainFace[] terrainFaces;
+
+    private void OnValidate()
+    {
+        Initialize();
+        GenerateMesh();
+        
+    }
+
+    void Initialize()
+    {
+        if(meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
+        
+        terrainFaces = new TerrainFace[6];
+        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+
+        for (int i = 0; i < meshFilters.Length; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                GameObject meshObj = new GameObject("mesh");
+                meshObj.transform.parent = transform;
+
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+            }
+
+            terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+        }
+    }
+
+    public void OnShapeSettingsUpdated()
+    {
+        Initialize();
+        GenerateMesh();
+    }
+
+    public void OnColorSettingsUpdated()
+    {
+        Initialize();
+        GenerateColor();
+    }
+
+    void GenerateMesh()
+    {
+        foreach (TerrainFace face in terrainFaces)
+        {
+            face.ConstructMesh();
+        }
+    }
+
+    void GenerateColor()
+    {
+        foreach (var m in meshFilters)
+        {
+            m.GetComponent<MeshRenderer>().sharedMaterial.color = colorSettings.planetColor;
+        }
+    }
+
+
+}
