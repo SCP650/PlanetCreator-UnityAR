bc1ea69c15befda4d8a6ed0d26f1c608f9e07bf9
Sebastian Yang <yukaiy@andrew.cmu.edu>
2022-08-20 10:05:00 -0700

    play with noise

--- Assets/_Scripts/Noise/NoiseSettings.cs
+++ Assets/_Scripts/Noise/NoiseSettings.cs
@@ -5,12 +5,32 @@ using UnityEngine;
 [System.Serializable]
 public class NoiseSettings 
 {
-    public float strength = 1;
-    [Range(1,8)]
-    public int numLayer = 1;
-    public float baseRoughness = 1;
-    public float roughtness = 2;
-    public float persistence = 0.5f;
-    public Vector3 center;
-    public float minValue;
+    public enum FilterType {Simple, Rigid};
+    public FilterType filterType;
+
+    [ConditionalHide("filterType",0)]
+    public SimpleNoiseSettings simpleNoiseSettings;
+    [ConditionalHide("filterType", 1)]
+    public RigidNoiseSettings rigidNoiseSettings;
+
+    [System.Serializable]
+    public class SimpleNoiseSettings
+    {
+        public float strength = 1;
+        [Range(1, 8)]
+        public int numLayer = 1;
+        public float baseRoughness = 1;
+        public float roughtness = 2;
+        public float persistence = 0.5f;
+        public Vector3 center;
+        public float minValue;
+    }
+
+    [System.Serializable]
+    public class RigidNoiseSettings : SimpleNoiseSettings
+    {
+        public float weightMultiplier = 0.8f;
+    }
+
+ 
 }
